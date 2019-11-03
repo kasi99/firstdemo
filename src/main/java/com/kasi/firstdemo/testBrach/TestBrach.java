fa1bb6342533c907ff295380a4233ee1f16ad444
kasi <ht333333@sina.com>
2019-11-04 06:05:05 +0800

    第一次提交

---
+++ src/main/java/com/kasi/firstdemo/testBrach/TestBrach.java
@@ -0,0 +1,16 @@
+package com.kasi.firstdemo.testBrach;
+
+/**
+ * <pre> TODO </pre>
+ *
+ * @Author: HuangTao
+ * @Date: 2019-11-04 06:01
+ * @Version:
+ */
+
+public class TestBrach {
+    public static void main(String[] args) {
+        System.out.println("用来测试分支合并，当前分支是develop_20191104");
+        System.out.println("用来测试分支合并，当前分支是develop_2019110401");
+    }
+}
