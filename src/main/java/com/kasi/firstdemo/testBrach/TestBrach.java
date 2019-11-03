069a3ef995e890e692ad74cf85745a8fec1d1abb
kasi <ht333333@sina.com>
2019-11-04 06:08:48 +0800

    第一次提交

---
+++ src/main/java/com/kasi/firstdemo/testBrach/TestBrach.java
@@ -0,0 +1,15 @@
+package com.kasi.firstdemo.testBrach;
+
+/**
+ * <pre> TODO </pre>
+ *
+ * @Author: HuangTao
+ * @Date: 2019-11-04 06:08
+ * @Version:
+ */
+
+public class TestBrach {
+    public static void main(String[] args) {
+        System.out.println("当前分支是：release_20191104");
+    }
+}
